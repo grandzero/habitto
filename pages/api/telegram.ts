561bdb6cb1dea105778ef46ec5c853293687ae38
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2024-08-04 14:29:52 +0300

    redis update

--- pages/api/telegram.ts
+++ pages/api/telegram.ts
@@ -1,24 +1,23 @@
 // pages/api/telegram.ts
-import type { NextApiRequest, NextApiResponse } from "next";
-import {
-  handleTelegramMessage,
-  handleCallbackQuery,
-} from "../../utils/handleTelegramMessage";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const { message, callback_query } = req.body;
 
     if (message) {
-      console.log("Incoming message:", message);
-      await handleTelegramMessage(message);
+      console.log("Received message:", message);
+      // Handle the message (e.g., reply to the user)
+      // await handleTelegramMessage(message);
     } else if (callback_query) {
-      console.log("Incoming callback query:", callback_query);
-      await handleCallbackQuery(callback_query);
+      console.log("Received callback query:", callback_query);
+      // Handle the callback query
+      // await handleCallbackQuery(callback_query);
     }
 
     res.status(200).end();
   } else {
-    res.status(200).send("Use POST method to interact with this bot.");
+    console.log(`Received a non-POST request: ${req.method}`);
+    res.status(404).end();
   }
 };
--- utils/redisClient.ts
+++ utils/redisClient.ts
@@ -2,7 +2,8 @@
 import Redis from "ioredis";
 
 const redisClient = new Redis(
-  process?.env?.REDIS_URL ?? "redis://localhost:6379"
+  process?.env?.REDIS_URL ?? "redis://localhost:6379",
+  { tls: {} }
 );
 
 redisClient.on("connect", () => {
