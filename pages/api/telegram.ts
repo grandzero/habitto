312814dfb474a7da0b739019f24bb44822695168
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2024-08-04 14:32:10 +0300

    receive and handle

--- pages/api/telegram.ts
+++ pages/api/telegram.ts
@@ -1,23 +1,24 @@
 // pages/api/telegram.ts
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  handleTelegramMessage,
+  handleCallbackQuery,
+} from "../../utils/handleTelegramMessage";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const { message, callback_query } = req.body;
 
     if (message) {
-      console.log("Received message:", message);
-      // Handle the message (e.g., reply to the user)
-      // await handleTelegramMessage(message);
+      console.log("Incoming message:", message);
+      await handleTelegramMessage(message);
     } else if (callback_query) {
-      console.log("Received callback query:", callback_query);
-      // Handle the callback query
-      // await handleCallbackQuery(callback_query);
+      console.log("Incoming callback query:", callback_query);
+      await handleCallbackQuery(callback_query);
     }
 
     res.status(200).end();
   } else {
-    console.log(`Received a non-POST request: ${req.method}`);
-    res.status(404).end();
+    res.status(200).send("Use POST method to interact with this bot.");
   }
 };
